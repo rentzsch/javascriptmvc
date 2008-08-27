f8b616b59218d1edeaf54f2cbdda8219a5e9fc0c
JustinBMeyer <JustinBMeyer@f352ecdf-183f-0410-ae60-f1561b901441>
2008-08-27 21:04:56 +0000

    added everything to help test documentation for everything
    git-svn-id: http://javascriptmvc.googlecode.com/svn/trunk@711 f352ecdf-183f-0410-ae60-f1561b901441

---
+++ apps/everything/compress.js
@@ -0,0 +1,19 @@
+MVCOptions = {
+    onload: false,
+    compress_callback: function(total){
+        var compressed = MVCOptions.collect_and_compress(total);
+        MVCOptions.save('apps/everything/production.js', compressed);
+        print("Compressed to 'apps/everything/production.js'.");
+        
+        print("Parsing files for docs ...");
+        var app = new MVCObject.DApplication(total, "everything");
+        
+        app.generate();
+        print("Generated docs.");
+    }
+}
+load('jmvc/rhino/htmlparser.js');
+load('jmvc/rhino/env.js');
+load('jmvc/rhino/helpers.js');
+load('jmvc/rhino/doc.js');
+window.location = 'apps/everything/index.html';
