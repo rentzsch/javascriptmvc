713c659b4b651d27ce1092bf030b3edc677c444b
JustinBMeyer <JustinBMeyer@f352ecdf-183f-0410-ae60-f1561b901441>
2008-08-27 21:05:16 +0000

    added everything to help test documentation for everything
    git-svn-id: http://javascriptmvc.googlecode.com/svn/trunk@712 f352ecdf-183f-0410-ae60-f1561b901441

---
+++ apps/everything.js
@@ -0,0 +1,24 @@
+include.resources();
+include.plugins('core','controller/comet','controller/dragdrop','dom/query','io/comet','io/jsonp','io/window_name',
+    'io/xdoc','lang/date','lang/json','model','model/ajax','model/cookie','model/jsonp','model/rest_json',
+    'model/rest_xml','model/view_helper');
+
+//dom/history
+
+
+include('../jmvc/rhino/doc');
+
+MVC.Initializer(function(){
+  include.models();
+  include.views();
+  
+  resp = new Ajax('../../jmvc/plugins/dom/event/standard.js?'+Math.random(),  {asynchronous: false, method: 'get'});
+  var file = new MVCObject.DFile({text:resp.responseText, path: "call.js" } );
+  
+  resp = new Ajax('../../jmvc/plugins/dom/element/vector.js?'+Math.random(),  {asynchronous: false, method: 'get'});
+  var file = new MVCObject.DFile({text:resp.responseText, path: "call.js" } );
+  //file.generate()
+  document.getElementById('CLASSIN').innerHTML = MVCObject.DConstructor.listing[0].toHTML();
+  
+  
+});
