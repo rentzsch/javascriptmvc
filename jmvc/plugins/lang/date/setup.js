7a3098033c3bf8ea60d32d6f33da5310e89e44f8
JustinBMeyer <JustinBMeyer@f352ecdf-183f-0410-ae60-f1561b901441>
2008-09-25 03:30:59 +0000

    added lee's patch
    git-svn-id: http://javascriptmvc.googlecode.com/svn/trunk@783 f352ecdf-183f-0410-ae60-f1561b901441

--- jmvc/plugins/lang/date/setup.js
+++ jmvc/plugins/lang/date/setup.js
@@ -1,8 +1,23 @@
 (function(){
 	var parse = Date.parse;
 	MVC.Native.extend('Date', {
+		add_days: function(date, number_of_days) {
+			date.setDate(date.getDate() + number_of_days);
+			return date;
+		},
+		add_weeks: function(date, number_of_weeks) {
+			return MVC.Date.add_days(date, number_of_weeks * 7);
+		},
+		day_name: function(date) {
+			return MVC.Date.day_names[date.getDay()];
+		},
+		first_day_of_week: function(date) {
+			var first_day = new Date(date);
+			first_day.setDate(date.getDate() - date.getDay());
+			return first_day;
+		},
 		month_name: function(date) {
-			return MVC.Date.month_names[date.getMonth()-1];
+			return MVC.Date.month_names[date.getMonth()];
 		},
 		number_of_days_in_month : function(date) {
 		    var year = date.getFullYear(),month = date.getMonth(),m = [31,28,31,30,31,30,31,31,30,31,30,31];
@@ -10,6 +25,7 @@
 		    if (year%4 != 0 || (year%100 == 0 && year%400 != 0)) return m[1];
 		    return m[1] + 1;
 		},
+		day_names: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
 		month_names: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
 		parse: function(data) {
 			if(typeof data != "string") return null;
