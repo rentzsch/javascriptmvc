cc347d9016dad7913c7e7885e6e6edc09b8948d5
brian.moschel <brian.moschel@f352ecdf-183f-0410-ae60-f1561b901441>
2008-09-26 04:45:29 +0000

    windows scripts accepts windows slashes (like js jmvc\scripts\controller Todo)
    git-svn-id: http://javascriptmvc.googlecode.com/svn/trunk@806 f352ecdf-183f-0410-ae60-f1561b901441

--- js.bat
+++ js.bat
@@ -1,6 +1,6 @@
 :: This script checks for arguments, if they don't exist it opens the Rhino dialog
 :: if arguments do exist, it loads the script in the first argument and passes the other arguments to the script
-:: ie: js jmvc/script/controller Todo
+:: ie: js jmvc\script\controller Todo
 @echo off
 SETLOCAL ENABLEDELAYEDEXPANSION
 if "%1"=="" (
@@ -8,11 +8,14 @@ if "%1"=="" (
 	GOTO END
 )
 SET ARGS=[
+SET FILENAME=%1
+SET FILENAME=%FILENAME:\=/%
+echo %FILENAME%
 ::haven't seen any way to loop through all args yet, so for now this goes through arg 2-7
 for %%a in (%2 %3 %4 %5 %6 %7) do (
 	if not "%%a"=="" SET ARGS=!ARGS!'%%a',
 )
 SET ARGS=%ARGS%]
-java -jar jmvc\rhino\js.jar -e _args=%ARGS% -e load('%1')
+java -jar jmvc\rhino\js.jar -e _args=%ARGS% -e load('%FILENAME%')
 
 :END
