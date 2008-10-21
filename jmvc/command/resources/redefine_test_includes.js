f768858054143dffe80975c7926f4fc1a7f10f0b
JustinBMeyer <JustinBMeyer@f352ecdf-183f-0410-ae60-f1561b901441>
2008-10-21 17:43:22 +0000

    Removing old generator console and some other old code.  Brought in OpenAjax hub for message passing between models and controllers.
    Moved around many files.
    
    Compression scripts need to be updated!
    
    git-svn-id: http://javascriptmvc.googlecode.com/svn/trunk@896 f352ecdf-183f-0410-ae60-f1561b901441

--- jmvc/command/resources/redefine_test_includes.js
+++
@@ -1,8 +0,0 @@
-MVC.Included.unit_tests = [];
-MVC.Included.functional_tests = [];
-include.unit_tests = function(){
-	for(var i=0; i < arguments.length; i++) MVC.Included.unit_tests.push(arguments[i].replace(/_test/,''));
-}
-include.functional_tests = function(){
-	for(var i=0; i < arguments.length; i++) MVC.Included.functional_tests.push(arguments[i].replace(/_test/,''));
-}
