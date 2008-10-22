28ddcd48ed1ac489b1c5d6c4674b473f711d0b35
JustinBMeyer <JustinBMeyer@f352ecdf-183f-0410-ae60-f1561b901441>
2008-10-22 23:38:20 +0000

    animation plugin and timer can do easings
    git-svn-id: http://javascriptmvc.googlecode.com/svn/trunk@903 f352ecdf-183f-0410-ae60-f1561b901441

---
+++ jmvc/plugins/dom/animate/animate.js
@@ -0,0 +1,82 @@
+MVC.Animate = function(element, params, duration, easing, callback){
+    callback = callback || function(){}
+    //get starting values.
+    var starting_values = {};
+    var val;
+    for(var param in params){
+        val = params[param];
+        starting_values[param] = new MVC.Animate.Value(element,param, val);
+        //this.get_starting_value( MVC.Element.get_style( element, param ) );
+    }
+    
+    this.timer = new MVC.Timer({from: 0, to: 1, time: duration,
+        onUpdate : function(percentage){
+            for(var param in starting_values){
+                element.style[param] = starting_values[param].get(percentage);
+            }
+        },
+        onComplete : function(){
+            for(var param in starting_values){
+                element.style[param] = starting_values[param].end();
+            }
+            callback(element);
+        }
+    });
+    this.timer.start();
+    
+}
+
+MVC.Animate.prototype = {
+    get_starting_value : function(val){
+        var parts = val.toString().match(/^([+-]=)?([\d+-.]+)(.*)$/), start = e.cur(true) || 0;
+		if ( parts ) {
+			var end = parseFloat(parts[2]), unit = parts[3] || "px";
+
+			// We need to compute starting value
+			if ( unit != "px" ) {
+				self.style[ name ] = (end || 1) + unit;
+				start = ((end || 1) / e.cur(true)) * start;
+				self.style[ name ] = start + unit;
+			}
+
+			// If a +=/-= token was provided, we're doing a relative animation
+			if ( parts[1] )
+				end = ((parts[1] == "-=" ? -1 : 1) * end) + start;
+
+			e.custom( start, end, unit );
+		} else
+			e.custom( start, val, "" );
+    }
+    
+};
+MVC.Animate.Value = function(element, style, end){
+    this.style = style;
+    this.start = parseFloat( MVC.Element.get_style( element, style )) || 0;
+    var parts = end.toString().match(/^([+-]=)?([\d+-.]+)(.*)$/);
+    
+    if ( parts ) {
+        this.end = parseFloat(parts[2]);
+        this.unit = parts[3] || "px";
+        if ( this.unit != "px" ) {
+				element.style[ name ] = (end || 1) + unit;
+				this.start = ((end || 1) / MVC.Element.get_style( element, style )) * start;
+				element.style[ name ] = start + unit;
+		}
+        if ( parts[1] )
+			this.end = ((parts[1] == "-=" ? -1 : 1) * this.end) + this.start;
+        
+    }else{
+        this.end = end;
+        this.unit = "px";
+    }
+    this.distance = this.end - this.start;
+}
+
+MVC.Animate.Value.prototype = {
+    get: function(percent){
+        return (this.start+percent*this.distance)+this.unit;
+    },
+    last: function(){
+        return (this.end)+this.unit;
+    }
+}
--- jmvc/plugins/lang/timer/setup.js
+++ jmvc/plugins/lang/timer/setup.js
@@ -1,12 +1,17 @@
 MVC.Timer = function(options){
     options = options || {};
-    this.time = options.time || 1000;
+    this.time = options.time || 500;
     this.from = options.from || 0;
     this.to = options.to || 1;
     this.interval = options.interval||1;
     this.update_callback = options.onUpdate || function(){};
     this.complete_callback = options.onCompete || function(){};
     this.distance = this.to - this.from;
+    if(options.easing){
+        this.easing = typeof options.easing == 'string' ? MVC.Timer.easings[options.easing] : options.easing;
+    }else{
+        this.easing = MVC.Timer.easings.swing
+    }
 }
 
 MVC.Timer.prototype = {
@@ -28,10 +33,20 @@ MVC.Timer.prototype = {
             this.kill();
             this.complete_callback(current);
         }else{
-            var how_far = time_passed / this.time;
-            current = this.from + (how_far * this.distance);
+            var percentage = time_passed / this.time;
+            current = this.easing(percentage, time_passed, this.from, this.distance  );
+            //current = this.from + (how_far * this.distance);
             this.update_callback(current);
         }
         
     }
+}
+
+MVC.Timer.easings = {
+	linear: function( p, n, firstNum, diff ) {
+		return firstNum + diff * p;
+	},
+	swing: function( p, n, firstNum, diff ) {
+		return ((-Math.cos(p*Math.PI)/2) + 0.5) * diff + firstNum;
+	}
 }
