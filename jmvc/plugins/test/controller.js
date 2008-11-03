966ca62849ab56c6e992734956e14e76012d58a5
brian.moschel <brian.moschel@f352ecdf-183f-0410-ae60-f1561b901441>
2008-11-03 20:10:22 +0000

    test doesn't break if you have a hover action
    git-svn-id: http://javascriptmvc.googlecode.com/svn/trunk@959 f352ecdf-183f-0410-ae60-f1561b901441

--- jmvc/plugins/test/controller.js
+++ jmvc/plugins/test/controller.js
@@ -24,7 +24,8 @@ MVC.Test.Controller = MVC.Test.Functional.extend({
 			var event_type = actions[action_name].event_type;
 			var cleaned_name = actions[action_name].css_selector.replace(/\.|#/g, '')+' '+event_type;
 			var helper_name = cleaned_name.replace(/(\w*)/g, function(m,part){ return MVC.String.capitalize(part)}).replace(/ /g, '');
-			helpers[helper_name] = helpers[MVC.String.capitalize(event_type)].curry(actions[action_name].css_selector);
+			if(helpers[MVC.String.capitalize(event_type)])
+				helpers[helper_name] = helpers[MVC.String.capitalize(event_type)].curry(actions[action_name].css_selector);
 			this.added_helpers[helper_name] = helpers[helper_name];
 		}
 		return helpers;
