b262a438e68efc362678a2f599911bdb58bf4336
JustinBMeyer <JustinBMeyer@f352ecdf-183f-0410-ae60-f1561b901441>
2008-12-10 07:48:34 +0000

    adds find and is_empty
    git-svn-id: http://javascriptmvc.googlecode.com/svn/trunk@1160 f352ecdf-183f-0410-ae60-f1561b901441

--- jmvc/plugins/model/simple_store.js
+++ jmvc/plugins/model/simple_store.js
@@ -38,17 +38,31 @@ MVC.Store = MVC.Class.extend(
     destroy: function(id){
 		delete this._data[id];
 	},
-    
+    /**
+     * Finds instances using a test function.  If no test function is provided returns all instances.
+     * @param {Function} f
+     * @return {Array}
+     */
     find : function(f){
         var instances = [];
         for(var id in this._data){
             var inst = this._data[id];
-            if(f(inst))
+            if(!f || f(inst))
                 instances.push(inst);
         }
         return instances;
     },
+    /**
+     * Clears instances
+     */
     clear : function(){
         this._data = {};
-    }
+    },
+    /**
+     * Returns if there is no instances
+     * @return {Boolean}
+     */
+    is_empty: function() {
+		return !this.find().length;
+	}
 });
