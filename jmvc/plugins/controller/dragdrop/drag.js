21948b352b969acd8901f95a4ef8dfafdedd3b54
JustinBMeyer <JustinBMeyer@f352ecdf-183f-0410-ae60-f1561b901441>
2008-12-10 03:22:13 +0000

    should not allow right mouse drag
    git-svn-id: http://javascriptmvc.googlecode.com/svn/trunk@1155 f352ecdf-183f-0410-ae60-f1561b901441

--- jmvc/plugins/controller/dragdrop/drag.js
+++ jmvc/plugins/controller/dragdrop/drag.js
@@ -97,8 +97,9 @@ MVC.Controller.Action.Drag = MVC.Controller.Action.Event.extend(
 	 * Gathers all callback functions and creates a new Draggable.
 	 */
 	mousedown : function(element, params){
+       var isLeftButton = params.event.which == 1;
        var jmvc= MVC.Delegator.jmvc(element);
-       if(jmvc.responding == false) return;
+       if(jmvc.responding == false || isLeftButton) return;
        var drag = jmvc.custom.drag
        MVC.Object.extend(params, drag[this.selector()].callbacks)
        if(MVC.Draggable.current) return;
