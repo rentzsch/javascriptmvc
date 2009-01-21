2d6ba5db6fdd63fd6ff3ce687f4b0747bf3d80a5
JustinBMeyer <JustinBMeyer@f352ecdf-183f-0410-ae60-f1561b901441>
2009-01-21 07:12:22 +0000

    opacity should work
    git-svn-id: http://javascriptmvc.googlecode.com/svn/branches/1_5@1263 f352ecdf-183f-0410-ae60-f1561b901441

--- jmvc/plugins/dom/animate/animate.js
+++ jmvc/plugins/dom/animate/animate.js
@@ -47,9 +47,9 @@ MVC.Animate.Value = function(element, style, end){
         this.end = parseFloat(parts[2]);
         this.unit = parts[3] || (MVC.Animate.exclude.test(style) ? "" : "px");
         if ( this.unit != "px" ) {
-				element.style[ name ] = (end || 1) + this.unit;
+				element.style[ style ] = (end || 1) + this.unit;
 				this.start = ((end || 1) / MVC.Element.get_style( element, style )) * start;
-				element.style[ name ] = start + this.unit;
+				element.style[ style ] = start + this.unit;
 		}
         if ( parts[1] )
 			this.end = ((parts[1] == "-=" ? -1 : 1) * this.end) + this.start;
