9b449086295652b4cc235d9eaa09c18993702028
JustinBMeyer <JustinBMeyer@f352ecdf-183f-0410-ae60-f1561b901441>
2009-02-12 20:32:41 +0000

    timer documentation
    git-svn-id: http://javascriptmvc.googlecode.com/svn/branches/1_5@1311 f352ecdf-183f-0410-ae60-f1561b901441

--- jmvc/plugins/lang/timer/setup.js
+++ jmvc/plugins/lang/timer/setup.js
@@ -1,8 +1,66 @@
+//some of this code comes from jQuery
 /**
  * @constructor
- * Creates a simple timer class
+ * Provides simple timing functionality.
+ * @code_start
+ * var t = new MVC.Timer({
+ *   time: 1000,
+ *   from:  1,
+ *   to: -1,
+ *   interval: 5,
+ *   onUpdate : function(value){ ... }
+ *   onComplete : function(value){ ... }
+ *   easing: "linear"
+ * })
+ * t.start();
+ * @code_end
  * @init
  * @param {Object} options
+ * <table class="options">
+					<tbody><tr><th>Option</th><th>Default</th><th>Description</th></tr>
+					<tr>
+						<td>time</td>
+						<td>500</td>
+						<td>Time the timer runs in ms.
+						</td>
+					</tr>
+					<tr>
+						<td>from</td>
+						<td>0</td>
+						<td>The starting value of the timer
+						</td>
+					</tr>
+					<tr>
+						<td>to</td>
+						<td>1</td>
+						<td>The end value of the timer.
+						</td>
+					</tr>
+					<tr>
+						<td>interval</td>
+						<td>1</td>
+						<td>Millisecond time between calls.  
+						</td>
+					</tr>
+					<tr>
+						<td>onUpdate</td>
+						<td></td>
+						<td>Called each time a new value is ready.
+						</td>
+					</tr>
+					<tr>
+						<td>onComplete</td>
+						<td> </td>
+						<td>Called when the timer is complete.
+						</td>
+					</tr>
+					<tr>
+						<td>easing</td>
+						<td>"swing"</td>
+						<td>The name of an easing or easing function.
+						</td>
+					</tr>
+				</tbody></table>
  */
 MVC.Timer = function(options){
     options = options || {};
@@ -21,11 +79,17 @@ MVC.Timer = function(options){
 }
 
 MVC.Timer.prototype = {
+    /**
+     * Starts the timer.
+     */
     start: function(){
         this.start_time = new Date();
 
         this.timer = setInterval(  MVC.Function.bind(this.next_step, this)  ,this.interval);
     },
+    /**
+     * Kills the timer.
+     */
     kill: function(){
         clearInterval(this.timer);
     },
@@ -47,11 +111,20 @@ MVC.Timer.prototype = {
         
     }
 }
-
+/**
+ * @class MVC.Timer.Easings
+ * Provides different easings that can be used to control how animations and timers change their values.
+ */
 MVC.Timer.Easings = {
+    /**
+     * Linear easing
+     */
 	linear: function( p, n, firstNum, diff ) {
 		return firstNum + diff * p;
 	},
+    /**
+     * Sinusoidal easing
+     */
 	swing: function( p, n, firstNum, diff ) {
 		return ((-Math.cos(p*Math.PI)/2) + 0.5) * diff + firstNum;
 	}
