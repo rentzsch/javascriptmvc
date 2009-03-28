c6ddd48f8a702be537789e39eae2933e455cf64e
JustinBMeyer <JustinBMeyer@f352ecdf-183f-0410-ae60-f1561b901441>
2009-03-28 18:08:08 +0000

    on another line
    git-svn-id: http://javascriptmvc.googlecode.com/svn/branches/1_5@1406 f352ecdf-183f-0410-ae60-f1561b901441

--- jmvc/plugins/controller/stateful/stateful_controller.js
+++ jmvc/plugins/controller/stateful/stateful_controller.js
@@ -49,7 +49,10 @@ MVC.Controller.Stateful = MVC.Controller.extend(
      */
     init: function(element){
         //check that this already isn't being delegated on:
-        if(element._mvc_controlled) throw "Element in "+this.Class.className+" controller is already wrapped by another controller."
+        if(element._mvc_controlled){
+            throw "Element in "+this.Class.className+" controller is already wrapped by another controller."
+        }
+            
         element._mvc_controlled = true;
         
         //needs to go through prototype, and attach events to this instance
