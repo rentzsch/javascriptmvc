857b20de65ac6a6187643a5e02e562fa8e90227e
lee.m.henson <lee.m.henson@f352ecdf-183f-0410-ae60-f1561b901441>
2009-04-15 10:25:33 +0000

    Added test to demonstrate problem with $([el1, el2]).appendTo(el3).
    git-svn-id: http://javascriptmvc.googlecode.com/svn/trunk@1454 f352ecdf-183f-0410-ae60-f1561b901441

--- test/unit/view_test.js
+++ test/unit/view_test.js
@@ -11,8 +11,19 @@ jQuery.Test.Unit.extend("Tests.View",{
         this.assertEqual('<h1>helloworld</h1>', $('#testarea').html().toLowerCase() );
         $('#testarea').html("");
 
+	},
+    test_appendTo_in_multiple_element_context : function(){
+		$('#testarea').html("");
+		
+		var content = $('<h1>helloworld</h1>');
+		var content2 = $('<h2>helloworld 2</h2>');
+        $([content, content2]).appendTo('#testarea');
+		
+		this.assertEqual(1, $('#testarea h1').length, 'h1 not found');
+		this.assertEqual(1, $('#testarea h2').length, 'h2 not found');
+		
+		//$('#testarea').html("");
 	}
-    
     /*,
 	test_render : function(){
 		this.assert_equal("<h1>yes</h1>\n\n<p>1</p>\n\n<p>2</p>\n\n<p>3</p>\n", new MVC.View({url: 'views/no_helpers'}).render( {data: {title: 'yes', info: [1,2,3]}}) );
